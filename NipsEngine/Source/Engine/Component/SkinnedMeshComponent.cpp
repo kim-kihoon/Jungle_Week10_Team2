aa96f7cde959dcb2c6f1d5c97e24ec13faf4db31
LEE SangHoon <hoon15x@gmail.com>
2026-05-09 16:29:04 +0900

    refactor: component split
    코드 꼬라지 보니 이제 슬슬 불편해져서 파일 하나에 몰려 있던 skeletalMesh 와 skinnedMesh 를 분리합니다.

--- NipsEngine/Source/Engine/Component/SkinnedMeshComponent.cpp
+++ NipsEngine/Source/Engine/Component/SkinnedMeshComponent.cpp
@@ -9,8 +9,6 @@
 #include "Core/ResourceManager.h"
 
 DEFINE_CLASS(USkinnedMeshComponent, UMeshComponent)
-DEFINE_CLASS(USkeletalMeshComponent, USkinnedMeshComponent)
-REGISTER_FACTORY(USkeletalMeshComponent)
 
 void USkinnedMeshComponent::SetSkeletalMesh(USkeletalMesh* InSkeletalMesh)
 {
@@ -416,84 +414,4 @@ void USkinnedMeshComponent::EnsureBoundsUpdated() const
     }
 
     const_cast<USkinnedMeshComponent*>(this)->UpdateWorldAABB();
-}
-
-void USkeletalMeshComponent::RefreshBoneTransforms()
-{
-    // 지금은 animation pose가 없으므로 reference pose 그대로 사용한다.
-    // 나중에 animation system이 생기면 여기서 local pose를 먼저 갱신한 뒤
-    // component-space로 누적하면 된다.
-    USkinnedMeshComponent::RefreshBoneTransforms();
-}
-
-void USkeletalMeshComponent::TickComponent(float DeltaTime)
-{
-    UMeshComponent::TickComponent(DeltaTime);
-
-    if (bBoneTransformsDirty || bSkinningDirty)
-    {
-        ComputeSkinnedVertices();
-    }
-}
-
-void USkeletalMeshComponent::Serialize(FArchive& Ar)
-{
-    UMeshComponent::Serialize(Ar);
-
-    Ar << "SkeletalMeshAsset" << SkeletalMeshAssetPath;
-
-    if (Ar.IsLoading())
-    {
-        TArray<UMaterialInterface*> SavedMaterials = Materials;
-
-        if (!SkeletalMeshAssetPath.empty())
-        {
-            SetSkeletalMesh(FResourceManager::Get().LoadSkeletalMesh(SkeletalMeshAssetPath));
-        }
-        else
-        {
-            SetSkeletalMesh(nullptr);
-        }
-
-        const int32 RestoreCount =
-            static_cast<int32>(std::min(SavedMaterials.size(), Materials.size()));
-
-        for (int32 i = 0; i < RestoreCount; ++i)
-        {
-            if (SavedMaterials[i] != nullptr)
-            {
-                SetMaterial(i, SavedMaterials[i]);
-            }
-        }
-    }
-}
-
-void USkeletalMeshComponent::GetEditableProperties(TArray<FPropertyDescriptor>& OutProps)
-{
-    UMeshComponent::GetEditableProperties(OutProps);
-    OutProps.push_back({ "SkeletalMesh", EPropertyType::String, &SkeletalMeshAssetPath });
-}
-
-void USkeletalMeshComponent::PostEditProperty(const char* PropertyName)
-{
-    UMeshComponent::PostEditProperty(PropertyName);
-
-    if (std::strcmp(PropertyName, "SkeletalMesh") == 0)
-    {
-        if (SkeletalMeshAssetPath.empty())
-        {
-            SetSkeletalMesh(nullptr);
-            return;
-        }
-
-        USkeletalMesh* Mesh = FResourceManager::Get().LoadSkeletalMesh(SkeletalMeshAssetPath);
-        SetSkeletalMesh(Mesh);
-    }
-    else if (std::strcmp(PropertyName, "Materials") == 0)
-    {
-        for (int32 i = 0; i < static_cast<int32>(Materials.size()); ++i)
-        {
-            SetMaterial(i, Materials[i]);
-        }
-    }
 }
--- NipsEngine/Source/Engine/Component/SkinnedMeshComponent.h
+++ NipsEngine/Source/Engine/Component/SkinnedMeshComponent.h
@@ -60,21 +60,3 @@ protected:
 	bool bRenderStateDirty = true;
 	bool bConsumedRenderStateDirty = false;
 };
-
-/* ―――― USkeletalMeshComponent ―――― */
-class USkeletalMeshComponent : public USkinnedMeshComponent
-{
-public:
-	DECLARE_CLASS(USkeletalMeshComponent, USkinnedMeshComponent)
-
-	USkeletalMeshComponent() = default;
-	~USkeletalMeshComponent() override = default;
-
-	void RefreshBoneTransforms() override;
-	void TickComponent(float DeltaTime) override;
-	void Serialize(FArchive& Ar) override;
-	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
-	void PostEditProperty(const char* PropertyName) override;
-
-private:
-};
